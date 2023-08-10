da812cb69f6df836efeb8707dabcdd17e6dfda07
CHICKIINY <bb.bright36389@gmail.com>
2023-08-10 15:08:34 +0700

    damn

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,24 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = ({ image, username, text, like }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src={image}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">{username}</span>
+      </div>
+
+      <span className="text-white">{text}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>{like}</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
